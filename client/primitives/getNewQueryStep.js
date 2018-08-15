e07bd6db6e449e525a60afa666cea03e44d2e432
Ismael Bejarano <ismael.bejarano@coinfabrik.com>
2018-08-15 18:21:57 -0300

    Fix comparison operator

--- client/primitives/getNewQueryStep.js
+++ client/primitives/getNewQueryStep.js
@@ -12,7 +12,7 @@ module.exports = async (api, sessionID) => {
 
   const result = await api.getResult(session.input, medStep)
 
-  if (result.stateHash == session.medHash) {
+  if (result.stateHash === session.medHash) {
     // we agree with their state; look in the right half
     return calculateMidpoint(medStep, highStep)
   } else {
